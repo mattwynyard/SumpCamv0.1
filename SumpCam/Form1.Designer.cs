a139b2127e54669add38c9dd6c3742c218751e7d
mattwynyard <mjwynyard@gmail.com>
2019-10-17 13:05:22 +1300

    Add project files.

---
+++ SumpCam/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace SumpCam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.boxPicture = new System.Windows.Forms.PictureBox();
+            this.btnPicture = new System.Windows.Forms.Button();
+            this.btnIntialise = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.boxPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // boxPicture
+            // 
+            this.boxPicture.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.boxPicture.Location = new System.Drawing.Point(27, 12);
+            this.boxPicture.Name = "boxPicture";
+            this.boxPicture.Size = new System.Drawing.Size(1087, 622);
+            this.boxPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.boxPicture.TabIndex = 0;
+            this.boxPicture.TabStop = false;
+            // 
+            // btnPicture
+            // 
+            this.btnPicture.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPicture.Location = new System.Drawing.Point(1131, 525);
+            this.btnPicture.Name = "btnPicture";
+            this.btnPicture.Size = new System.Drawing.Size(188, 109);
+            this.btnPicture.TabIndex = 1;
+            this.btnPicture.Text = "Click";
+            this.btnPicture.UseVisualStyleBackColor = true;
+            this.btnPicture.Click += new System.EventHandler(this.btnPicture_Click);
+            // 
+            // btnIntialise
+            // 
+            this.btnIntialise.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIntialise.Location = new System.Drawing.Point(1162, 379);
+            this.btnIntialise.Name = "btnIntialise";
+            this.btnIntialise.Size = new System.Drawing.Size(128, 50);
+            this.btnIntialise.TabIndex = 2;
+            this.btnIntialise.Text = "Intialise";
+            this.btnIntialise.UseVisualStyleBackColor = true;
+            this.btnIntialise.Click += new System.EventHandler(this.btnIntialise_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1349, 695);
+            this.Controls.Add(this.btnIntialise);
+            this.Controls.Add(this.btnPicture);
+            this.Controls.Add(this.boxPicture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.boxPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox boxPicture;
+        private System.Windows.Forms.Button btnPicture;
+        private System.Windows.Forms.Button btnIntialise;
+    }
+}
+
---
+++ SumpCam/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace SumpCam
+{
+    public partial class Form1 : Form
+    {
+
+        VideoCapture mCapture;
+        Boolean play = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            intialize();
+
+        }
+
+        public void intialize()
+        {
+            //mCapture = new VideoCapture("Z:\\Matt\\Buddy Guy, Ronnie Wood & Johnny Lang Miss You Crossroads Guitar Festival 2010.mp4");
+            mCapture = new VideoCapture(0);
+            if (mCapture.IsOpened)
+            {
+                const string message = "Video capture has been opened correctly";
+                const string caption = "Form";
+                var result = MessageBox.Show(message, caption,
+                                             MessageBoxButtons.OK,
+                                             MessageBoxIcon.Information);
+
+                // If the no button was pressed ...
+                if (result == DialogResult.OK)
+                {
+                    openStream(mCapture);
+                    //Close();
+
+                }
+            } else
+            {
+                const string message = "Video capture is closed";
+                const string caption = "Form";
+                var result = MessageBox.Show(message, caption,
+                                             MessageBoxButtons.OK,
+                                             MessageBoxIcon.Exclamation);
+
+                // If the no button was pressed ...
+                if (result == DialogResult.OK)
+                {
+                    // cancel the closure of the form.
+                    //Close();
+                }
+            }
+
+        }
+
+        public void openStream(VideoCapture capture)
+        {
+            //Boolean grab = mCapture.Grab();
+            int height = mCapture.Height;
+            int width = mCapture.Width;
+            Boolean grab = mCapture.Grab();
+            Mat frame = new Mat(height, width, DepthType.Cv8U, 3);
+            mCapture.Read(frame);
+            Image<Bgr, Byte> image = frame.ToImage<Bgr, Byte>();
+            boxPicture.Image = ImageFromEMGUImage(image);
+            
+
+        }
+
+        public static Image ImageFromEMGUImage(Image<Bgr, Byte> image)
+        {
+            MemoryStream ms = new MemoryStream();
+            Image bitmap = image.ToBitmap();
+            image.Dispose();
+            bitmap.Save(ms, ImageFormat.Jpeg);
+            bitmap.Dispose();
+            ms.Seek(0, SeekOrigin.Begin);
+            return Image.FromStream(ms);
+        }
+
+        private void btnPicture_Click(object sender, EventArgs e)
+        {
+            if (play)
+            {
+                play = false;
+                return;
+            } else
+            {
+                play = true;
+            }
+            
+
+        }
+
+        private void btnIntialise_Click(object sender, EventArgs e)
+        {
+            
+            openStream(mCapture);
+        }
+    }
+}
